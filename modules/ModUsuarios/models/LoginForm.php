05a32862a8f9427d21b9ba5117e82113eeaa60c6
cloud-elric <humberto@2gom.com.mx>
2017-07-17 16:29:08 -0500

    Cambiando mensajes a español de login

--- modules/ModUsuarios/models/LoginForm.php
+++ modules/ModUsuarios/models/LoginForm.php
@@ -31,6 +31,7 @@ class LoginForm extends Model {
 								'password' 
 						],
 						'required',
+						'message'=>"Campo requerido",
 						'on' => 'login' 
 				],
 				// username es requerido para recuperar la contraseña
