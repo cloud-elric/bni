7c54265fa3743ea98e09d3a4dc499c1f0b74ccb0
cloud-elric <humberto@2gom.com.mx>
2017-07-17 16:04:39 -0500

    Modificando login

--- modules/ModUsuarios/views/manager/login.php
+++ modules/ModUsuarios/views/manager/login.php
@@ -126,31 +126,37 @@ window.fbAsyncInit = function() {
 <div class="site-login">
 	<div class="row">
 		<div class="col-md-6 col-md-offset-3">
-			<h1><?= Html::encode($this->title) ?></h1>
-			<?php $form = ActiveForm::begin([
-				'id' => 'login-form',
-				//'options' => ['class' => 'form-horizontal'],
-				'fieldConfig' => [
-					//'template' => "{label}\n<div class=\"col-lg-8\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
-					//'labelOptions' => ['class' => 'col-lg-3 control-label'],
-				],
-			]); ?>
 
-				<?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+			<div class="panel">
+				<div class="panel-body">
+				
+					<h1 class="text-center"><?= Html::encode($this->title) ?></h1>
+					<?php $form = ActiveForm::begin([
+						'id' => 'login-form',
+						//'options' => ['class' => 'form-horizontal'],
+						'fieldConfig' => [
+							//'template' => "{label}\n<div class=\"col-lg-8\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
+							//'labelOptions' => ['class' => 'col-lg-3 control-label'],
+						],
+					]); ?>
 
-				<?= $form->field($model, 'password')->passwordInput() ?>
+						<?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
 
-				<?= $form->field($model, 'rememberMe')->checkbox([
-					'template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
-				]) ?>
+						<?= $form->field($model, 'password')->passwordInput() ?>
 
-				<div class="form-group">
-					<div class="col-lg-offset-1 col-lg-11">
-						<?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-					</div>
+						<?= $form->field($model, 'rememberMe')->checkbox([
+							'template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
+						]) ?>
+
+						<div class="form-group">
+							<div class="col-lg-offset-1 col-lg-11">
+								<?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+							</div>
+						</div>
+
+					<?php ActiveForm::end(); ?>	
 				</div>
-
-			<?php ActiveForm::end(); ?>	
+			</div>		
    		</div>
 	</div>
 </div>
