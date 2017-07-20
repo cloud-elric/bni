8b823c8b54638249b704a6b01cce43fec3360680
Raul Alanis <rulosc07@gmail.com>
2017-07-20 17:19:47 -0500

    cracion de action para ver datos despues de mandar correo

---
+++ views/site/view-lead.php
@@ -0,0 +1,15 @@
+<div class="row">
+  <div class="col-md-4">
+    <h3>Nombre</h3>
+    <p><?= $lead->txt_nombre_contacto ?></p>
+  </div>
+  <div class="col-md-4">
+    <h3>Telefono</h3>
+    <p><?= $lead->txt_numero_contacto ?></p>
+  </div>
+  <div class="col-md-4">
+    <h3>Notas</h3>
+    <p><?= $lead->txt_descripcion ?></p>
+  </div>
+  <button class="btn btn-primary">Save</button>
+</div>
